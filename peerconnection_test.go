a443519a518035347a8fb98eb8ce11bafe3693fd
Philipp Hancke <philipp.hancke@googlemail.com>
2025-12-25 09:46:20 +0100

    WARP test

--- peerconnection_test.go
+++ peerconnection_test.go
@@ -4,6 +4,7 @@
 package webrtc
 
 import (
+	"strings"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -837,4 +838,39 @@ func TestICETrickleCapabilityString(t *testing.T) {
 	for _, tt := range tests {
 		assert.Equal(t, tt.expected, tt.value.String())
 	}
+}
+
+func TestWarp(t *testing.T) {
+	offer, answer, err := newPair()
+	assert.NoError(t, err)
+
+	peerConnectionsConnected := untilConnectionState(PeerConnectionStateConnected, offer, answer)
+	assert.NoError(t, signalPair(offer, answer))
+	peerConnectionsConnected.Wait()
+
+	closePairNow(t, offer, answer)
+}
+
+func TestWarpClient(t *testing.T) {
+	s := SettingEngine{}
+	s.EnableSped(true)
+	api := NewAPI(WithSettingEngine(s))
+
+	offer, err := api.NewPeerConnection(Configuration{})
+	assert.NoError(t, err)
+	answer, err := api.NewPeerConnection(Configuration{})
+	assert.NoError(t, err)
+
+	peerConnectionsConnected := untilConnectionState(PeerConnectionStateConnected, offer, answer)
+	assert.NoError(t, signalPairWithModification(
+		offer, answer,
+		func(sessionDescription string) string {
+			return strings.ReplaceAll(
+				sessionDescription,
+				"setup:actpass",
+				"setup:active")
+		}))
+	peerConnectionsConnected.Wait()
+
+	closePairNow(t, offer, answer)
 }
